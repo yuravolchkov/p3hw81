c373939595fef9b006aade543556a4dad3c2bb4b
WhiteMasterYuraCPP-20 <yuravolchkov@gmail.com>
2024-06-09 23:25:15 +0300

    first Commit

---
+++ HW8.1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <exception>
+
+int function(std::string str, int forbidden_length)
+{
+    if (str.length() == forbidden_length)
+    {
+        throw "bad_length";
+    }
+    else
+    {
+        return str.length();
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // задать русский текст
+    std::system("chcp 1251"); // настроить кодировку консоли
+    std::system("cls"); // очистить экран
+
+    std::string str_for_user;
+    int forbidden_length, calc_length;
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+    for (bool i=0; i!=1;)
+    {
+        std::cout << "Введите слово: ";
+        std::cin >> str_for_user;
+        try
+        {
+            std::cout << "Длина слова " << str_for_user << " равна " << function(str_for_user, forbidden_length) << std::endl;
+        }
+        catch (const char*)
+        {
+            std::cout << "\nВы ввели слово запретной длины! До свидания\n";
+            i = 1;
+        }
+    }
+  return 81;
+}
